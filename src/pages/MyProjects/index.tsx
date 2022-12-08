3bca7677f1103975e1ae1e2322c07f2dd735562f
Charles Eduardo Mello Guimaraes <charles.eduardo.mello.guimaraes@hotmail.com>
2022-12-08 17:02:54 -0300

    feat: page projects

---
+++ src/pages/MyProjects/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function () {
+    return (
+        <div style={{ backgroundColor: "blue", width: "100vw", height: "100vh" }}>
+
+
+
+        </div>
+    )
+}
