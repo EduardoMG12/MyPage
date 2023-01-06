c0bfad9530ab9f21331f1a8a9549de6c01f1ce40
Charles Eduardo Mello Guimaraes <charles.eduardo.mello.guimaraes@hotmail.com>
2023-01-06 16:02:20 -0300

    refactor(Carousel): refactoring Carousel with the use of props, add new components CardProject, ICarousel, CCarousel

---
+++ src/components/Carousel/CCarousel/index.tsx
@@ -0,0 +1,20 @@
+import React, { Children, useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { ContainerCarousel } from './style';
+import ICarousel from '../ICarousel'
+
+type ICCarousel = {
+    width?: string,
+    bgColor?: string,
+    children: React.ReactNode,
+}
+
+const CCarousel: React.FC<ICCarousel> = ({ width, bgColor, children }) => {
+    return (
+        <ContainerCarousel style={{ width: `${width ?? '100%'}`, backgroundColor: `${bgColor ?? ''}` }}>
+            {children}
+        </ContainerCarousel >
+    )
+};
+
+export default CCarousel;
