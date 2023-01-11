caf54c4692ac827870ca20474a4b0b15fca98ffb
Charles Eduardo Mello Guimaraes <charles.eduardo.mello.guimaraes@hotmail.com>
2023-01-11 17:23:49 -0300

    fix()

--- src/components/Carousel/ItemCarousel/style.tsx
+++ src/components/Carousel/ItemCarousel/style.tsx
@@ -12,7 +12,7 @@ export const CardItemCarousel = styled.div<IItemCarousel>`
 `;
 
 export const Image = styled.div<IItemCarousel>`
-    background-image: url(${(props) => props.bgImage?.length > 1 ? props.bgImage : 'https //source.unsplash.com/random/300x300px?developer'});
+    background-image: url(${(props) => (props.bgImage != undefined && props.bgImage?.length) > 1 ? props.bgImage : 'https //source.unsplash.com/random/300x300px?developer'});
     min-width:100%;
     height: 50%;
     border-radius: 4rem 4rem 1rem 1rem;
