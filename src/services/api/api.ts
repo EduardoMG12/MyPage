29c1239e4263490a5774dc8c7df49d0abb8bcccf
EduardoMG12 <charles.eduardo.mello.guimaraes@hotmail.com>
2023-01-12 16:01:38 -0300

    refactoring(api)

--- src/services/api/api.ts
+++ src/services/api/api.ts
@@ -1,5 +1,19 @@
+export type ProjectsType = {
+    [project: string]: IProjects[]
+}
+
+export interface IProjects {
+    id: string,
+    title: string,
+    image: string,
+    descriptionShort: string,
+    descriptionLong: string,
+    iconTecnologics: [IconTecnologyType],
+    codeAndDeploy: CodeDeployType
+}
+
 export type IconTecnologyType = {
-    class: string,
+    className: string,
     color: string
 }
 
@@ -8,56 +22,43 @@ export type CodeDeployType = {
     deploy: string
 }
 
-export interface IProjects {
-    id: string,
-    tittle: string,
-    image: string,
-    sinopseShort: string,
-    sinopseLong: string,
-    iconTecnologics: {
-        [tecnology: string]: IconTecnologyType
-    },
-    codeAndDeploy: CodeDeployType
-}
 
-export type ProjectsType = {
-    [project: string]: IProjects[]
-}
 
 const projects: ProjectsType =
 {
     "Projetos": [{
         "id": "1",
-        "tittle": "Projeto Orange Juice",
+        "title": "Projeto Orange Juice",
         "image": "projectsImages/orangeJuice.png",
-        "sinopseShort": "Um hackthon desenvolvido para uma entrevista de emprego...",
-        "sinopseLong": "Hackthon para empresa FCâmara após duas etapas para filtrar competidores(uma prova e uma entrevista) veio o projeto que teve como objetivo que nós fizessemos um site para OrangeJuice(empresa da Fcamara) seguindo certas regras assim testando nosso conhecimento e tendo o mesmo como portifolio, aonde eu, dois Desenvolvedores e uma UX Design, passamos quinze dias desenvolvendo um site que tinha como objetivo ter login de úsuarios e admins, 3 trilhas principais, entrando nas mesmas teriam vários cursos. O objetivo principal era entregar um crud, 3 páginas prontas responsivas. Sou muito grato por esse projeto, mesmo o mesmo não sendo concluido por falta de tempo e organização pois a maioria nunca tinha feito um projeto em equipe, senti ter ganho muito conhecimento e aprendizado nesses dias pois foi muito puxado nunca tinha me dedicado tanto a um projeto em tão pouco tempo, acho que ocorreu um grande desenvolvimento desde trabalho em grupo, sistema de branchs, react, insomnina...",
-        "iconTecnologics": {
-            "JavaScript": {
-                "class": "fa-brands fa-js",
+        "descriptionShort": "Um hackthon desenvolvido para uma entrevista de emprego...",
+        "descriptionLong": "Hackthon para empresa FCâmara após duas etapas para filtrar competidores(uma prova e uma entrevista) veio o projeto que teve como objetivo que nós fizessemos um site para OrangeJuice(empresa da Fcamara) seguindo certas regras assim testando nosso conhecimento e tendo o mesmo como portifolio, aonde eu, dois Desenvolvedores e uma UX Design, passamos quinze dias desenvolvendo um site que tinha como objetivo ter login de úsuarios e admins, 3 trilhas principais, entrando nas mesmas teriam vários cursos. O objetivo principal era entregar um crud, 3 páginas prontas responsivas. Sou muito grato por esse projeto, mesmo o mesmo não sendo concluido por falta de tempo e organização pois a maioria nunca tinha feito um projeto em equipe, senti ter ganho muito conhecimento e aprendizado nesses dias pois foi muito puxado nunca tinha me dedicado tanto a um projeto em tão pouco tempo, acho que ocorreu um grande desenvolvimento desde trabalho em grupo, sistema de branchs, react, insomnina...",
+        "iconTecnologics": [
+            {
+
+                "className": "fa-brands fa-js",
                 "color": "#EAD41C"
             },
-            "TypeScript": {
-                "class": "devicon-typescript-plain",
+            {
+                "className": "devicon-typescript-plain",
                 "color": "#2C9FD8"
             },
-            "Next": {
-                "class": "devicon-nextjs-original",
+            {
+                "className": "devicon-nextjs-original",
                 "color": "black"
             },
-            "React": {
-                "class": "fa-brands fa-react",
+            {
+                "className": "fa-brands fa-react",
                 "color": "#2C9FD8"
             },
-            "NODE": {
-                "class": "fa-brands fa-node-js",
+            {
+                "className": "fa-brands fa-node-js",
                 "color": "orange"
             },
-            "Mongo": {
-                "class": "devicon-mongodb-plain",
+            {
+                "className": "devicon-mongodb-plain",
                 "color": "green"
             }
-        },
+        ],
         "codeAndDeploy": {
             "code": "https://github.com/orgs/orange-squad28/repositories",
             "deploy": ""
@@ -65,21 +66,21 @@ const projects: ProjectsType =
     },
     {
         "id": "a78d78128",
-        "tittle": "Landing Page",
+        "title": "Landing Page",
         "image": "projectsImages/landingPage.png",
-        "sinopseShort": "landing page com uma breve historia sobre eu e aprensetaçao",
-        "sinopseLong": "Um projeto que fiz com o objetivo de treinar/testar meus conhecimentos de html css e js, um projeto simples porem funcional, que tem como premissa ser uma página contando um pouco da minha hístoria e me apresentando, mostrando minhas midias profissionais.",
+        "descriptionShort": "landing page com uma breve historia sobre eu e aprensetaçao",
+        "descriptionLong": "Um projeto que fiz com o objetivo de treinar/testar meus conhecimentos de html css e js, um projeto simples porem funcional, que tem como premissa ser uma página contando um pouco da minha hístoria e me apresentando, mostrando minhas midias profissionais.",
         "iconTecnologics": {
             "JavaScript": {
-                "class": "fa-brands fa-js",
+                "className": "fa-brands fa-js",
                 "color": "#EAD41C"
             },
             "HTML": {
-                "class": "fa-brands fa-html5",
+                "className": "fa-brands fa-html5",
                 "color": "#E56027"
             },
             "CSS": {
-                "class": "fa-brands fa-css3-alt",
+                "className": "fa-brands fa-css3-alt",
                 "color": "#2C9FD8"
             },
         },
@@ -94,21 +95,21 @@ const projects: ProjectsType =
 
 
         "id": "347892738yazwe4",
-        "tittle": "Busca de perfis do github por api",
+        "title": "Busca de perfis do github por api",
         "image": "projectsImages/fetchGitHubApi.png",
-        "sinopseShort": "Página que puxa o github de usúarios pela a api ",
-        "sinopseLong": "Uma página completamente responsiva com um input para puxar nick-names de usuários do github, este projeto foi feito vizando a organização e a separação de arquivos interface telas entre outras coisas para o código ficar mais limpo. ",
+        "descriptionShort": "Página que puxa o github de usúarios pela a api ",
+        "descriptionLong": "Uma página completamente responsiva com um input para puxar nick-names de usuários do github, este projeto foi feito vizando a organização e a separação de arquivos interface telas entre outras coisas para o código ficar mais limpo. ",
         "iconTecnologics": {
             "JavaScript": {
-                "class": "fa-brands fa-js",
+                "className": "fa-brands fa-js",
                 "color": "#EAD41C"
             },
             "HTML": {
-                "class": "fa-brands fa-html5",
+                "className": "fa-brands fa-html5",
                 "color": "#E56027"
             },
             "CSS": {
-                "class": "fa-brands fa-css3-alt",
+                "className": "fa-brands fa-css3-alt",
                 "color": "#2C9FD8"
             },
 
@@ -121,21 +122,21 @@ const projects: ProjectsType =
     },
     {
         "id": "347892738y4",
-        "tittle": "Timer",
+        "title": "Timer",
         "image": "projectsImages/timer.png",
-        "sinopseShort": "Cronômetro funcional",
-        "sinopseLong": "Um projeto simples porem funcional de um timer com três botões, reset para zerar o timer, pause para parar e init para começar a contar ou continuar, o projeto sendo funcional e responsivo.",
+        "descriptionShort": "Cronômetro funcional",
+        "descriptionLong": "Um projeto simples porem funcional de um timer com três botões, reset para zerar o timer, pause para parar e init para começar a contar ou continuar, o projeto sendo funcional e responsivo.",
         "iconTecnologics": {
             "JavaScript": {
-                "class": "fa-brands fa-js",
+                "className": "fa-brands fa-js",
                 "color": "#EAD41C"
             },
             "HTML": {
-                "class": "fa-brands fa-html5",
+                "className": "fa-brands fa-html5",
                 "color": "#E56027"
             },
             "CSS": {
-                "class": "fa-brands fa-css3-alt",
+                "className": "fa-brands fa-css3-alt",
                 "color": "#2C9FD8"
             },
 
@@ -148,21 +149,21 @@ const projects: ProjectsType =
     },
     {
         "id": "12378916045",
-        "tittle": "To-do List",
+        "title": "To-do List",
         "image": "projectsImages/toDoList.png",
-        "sinopseShort": "Uma lista de a fazeres que guarda dados em LocalStorage",
-        "sinopseLong": "O projeto ajuda a organizar as ideias o objetivo é ser uma lista de a fazeres aonde você mesmo consegue colocar e deletar itens, a mesma tem a função de deletar os a fazeres apertando no botão e de adicionar apertando no botão ou apertando enter. A página é completamente responsivel.",
+        "descriptionShort": "Uma lista de a fazeres que guarda dados em LocalStorage",
+        "descriptionLong": "O projeto ajuda a organizar as ideias o objetivo é ser uma lista de a fazeres aonde você mesmo consegue colocar e deletar itens, a mesma tem a função de deletar os a fazeres apertando no botão e de adicionar apertando no botão ou apertando enter. A página é completamente responsivel.",
         "iconTecnologics": {
             "JavaScript": {
-                "class": "fa-brands fa-js",
+                "className": "fa-brands fa-js",
                 "color": "#EAD41C"
             },
             "HTML": {
-                "class": "fa-brands fa-html5",
+                "className": "fa-brands fa-html5",
                 "color": "#E56027"
             },
             "CSS": {
-                "class": "fa-brands fa-css3-alt",
+                "className": "fa-brands fa-css3-alt",
                 "color": "#2C9FD8"
             },
         },
@@ -173,21 +174,21 @@ const projects: ProjectsType =
     },
     {
         "id": "12874781745",
-        "tittle": "Calculadora",
+        "title": "Calculadora",
         "image": "projectsImages/calculator.png",
-        "sinopseShort": "Calculadora feita em js com factory function",
-        "sinopseLong": "Um exercicio de um curso que fiz, que é um projeto feito em facroty function, fiz quando estava pegando a base do js o mesmo foi feito primeiramente em constructor function e depois refatorado para factory function, fiz uns updates da calculadora que não estava no projeto final, que foi a responsividade, o led em cima da calculadora o som que a calculadora faz e o aviso de erro.",
+        "descriptionShort": "Calculadora feita em js com factory function",
+        "descriptionLong": "Um exercicio de um curso que fiz, que é um projeto feito em facroty function, fiz quando estava pegando a base do js o mesmo foi feito primeiramente em constructor function e depois refatorado para factory function, fiz uns updates da calculadora que não estava no projeto final, que foi a responsividade, o led em cima da calculadora o som que a calculadora faz e o aviso de erro.",
         "iconTecnologics": {
             "JavaScript": {
-                "class": "fa-brands fa-js",
+                "className": "fa-brands fa-js",
                 "color": "#EAD41C"
             },
             "HTML": {
-                "class": "fa-brands fa-html5",
+                "className": "fa-brands fa-html5",
                 "color": "#E56027"
             },
             "CSS": {
-                "class": "fa-brands fa-css3-alt",
+                "className": "fa-brands fa-css3-alt",
                 "color": "#2C9FD8"
             },
         },
