9a988948240da94b83199916f64e9ff3e8a85107
EduardoMG12 <charles.eduardo.mello.guimaraes@hotmail.com>
2023-01-16 15:46:49 -0300

    fix(): Delete dir images and add urls from images projects

--- src/services/api/api.ts
+++ src/services/api/api.ts
@@ -30,7 +30,7 @@ const projects: ProjectsType =
     "Projetos": [{
         "id": "1",
         "title": "Projeto Orange Juice",
-        "image": "projectsImages/orangeJuice.png",
+        "image": "https://docs.google.com/uc?id=1I3kIAWfqOamABw3xyWOtK_jHpG6Do-Wn",
         "descriptionShort": "Um hackthon desenvolvido para uma entrevista de emprego...",
         "descriptionLong": "Hackthon para empresa FCâmara após duas etapas para filtrar competidores(uma prova e uma entrevista) veio o projeto que teve como objetivo que nós fizessemos um site para OrangeJuice(empresa da Fcamara) seguindo certas regras assim testando nosso conhecimento e tendo o mesmo como portifolio, aonde eu, dois Desenvolvedores e uma UX Design, passamos quinze dias desenvolvendo um site que tinha como objetivo ter login de úsuarios e admins, 3 trilhas principais, entrando nas mesmas teriam vários cursos. O objetivo principal era entregar um crud, 3 páginas prontas responsivas. Sou muito grato por esse projeto, mesmo o mesmo não sendo concluido por falta de tempo e organização pois a maioria nunca tinha feito um projeto em equipe, senti ter ganho muito conhecimento e aprendizado nesses dias pois foi muito puxado nunca tinha me dedicado tanto a um projeto em tão pouco tempo, acho que ocorreu um grande desenvolvimento desde trabalho em grupo, sistema de branchs, react, insomnina...",
         "iconTecnologics": [
@@ -74,7 +74,7 @@ const projects: ProjectsType =
     {
         "id": "a78d78128",
         "title": "Landing Page",
-        "image": "projectsImages/landingPage.png",
+        "image": "https://docs.google.com/uc?id=1HmGJzFwioWkvRb9BMG_D51GD5kSGz4Tq",
         "descriptionShort": "landing page com uma breve historia sobre eu e aprensetaçao",
         "descriptionLong": "Um projeto que fiz com o objetivo de treinar/testar meus conhecimentos de html css e js, um projeto simples porem funcional, que tem como premissa ser uma página contando um pouco da minha hístoria e me apresentando, mostrando minhas midias profissionais.",
         "iconTecnologics": [
@@ -102,11 +102,9 @@ const projects: ProjectsType =
     ],
     "JavaScript": [{
 
-
-
         "id": "347892738yazwe4",
         "title": "Busca de perfis do github por api",
-        "image": "projectsImages/fetchGitHubApi.png",
+        "image": "https://docs.google.com/uc?id=1NMotoboMaqvqDAy2bTlg1EdF5ZVq_nKy",
         "descriptionShort": "Página que puxa o github de usúarios pela a api ",
         "descriptionLong": "Uma página completamente responsiva com um input para puxar nick-names de usuários do github, este projeto foi feito vizando a organização e a separação de arquivos interface telas entre outras coisas para o código ficar mais limpo. ",
         "iconTecnologics": [
@@ -136,7 +134,7 @@ const projects: ProjectsType =
     {
         "id": "347892738y4",
         "title": "Timer",
-        "image": "projectsImages/timer.png",
+        "image": "https://docs.google.com/uc?id=1B2XkhxV8YLOUek_BzHv-cPiR84r_zmLT",
         "descriptionShort": "Cronômetro funcional",
         "descriptionLong": "Um projeto simples porem funcional de um timer com três botões, reset para zerar o timer, pause para parar e init para começar a contar ou continuar, o projeto sendo funcional e responsivo.",
         "iconTecnologics": [
@@ -166,7 +164,7 @@ const projects: ProjectsType =
     {
         "id": "12378916045",
         "title": "To-do List",
-        "image": "projectsImages/toDoList.png",
+        "image": "https://docs.google.com/uc?id=1i40foVeV2xbF7CuVXqwmSUZ2ltoDLtZY",
         "descriptionShort": "Uma lista de a fazeres que guarda dados em LocalStorage",
         "descriptionLong": "O projeto ajuda a organizar as ideias o objetivo é ser uma lista de a fazeres aonde você mesmo consegue colocar e deletar itens, a mesma tem a função de deletar os a fazeres apertando no botão e de adicionar apertando no botão ou apertando enter. A página é completamente responsivel.",
         "iconTecnologics": [
@@ -194,7 +192,7 @@ const projects: ProjectsType =
     {
         "id": "12874781745",
         "title": "Calculadora",
-        "image": "projectsImages/calculator.png",
+        "image": "https://docs.google.com/uc?id=1kfBW42SX_vlPYP4BYmUDjEexi3ZIoJ6l",
         "descriptionShort": "Calculadora feita em js com factory function",
         "descriptionLong": "Um exercicio de um curso que fiz, que é um projeto feito em facroty function, fiz quando estava pegando a base do js o mesmo foi feito primeiramente em constructor function e depois refatorado para factory function, fiz uns updates da calculadora que não estava no projeto final, que foi a responsividade, o led em cima da calculadora o som que a calculadora faz e o aviso de erro.",
         "iconTecnologics": [
