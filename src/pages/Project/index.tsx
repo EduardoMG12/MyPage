b5a16ac0806728a71bc0e9ff322d7a4a757cb621
EduardoMG12 <charles.eduardo.mello.guimaraes@hotmail.com>
2023-01-18 15:40:17 -0300

    feat(project/:id): add new dinamic route

--- src/pages/Project/index.tsx
+++ src/pages/Project/index.tsx
@@ -1,23 +1,45 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import projetos from '../../services/api/api';
+import projetos, { IProjects } from '../../services/api/api';
+import { Container, ImageBgd, TextContent } from './style';
 
 function Project() {
     const { id } = useParams();
-    const [project, setProject] = useState(null);
+    const [project, setProject] = useState<IProjects>(null!);
 
     useEffect(() => {
-        console.log(projetos)
-        const currentProject = projetos.prototype.find(project => project.id === id);
-        setProject(currentProject)
+        const currentProject = Object.values(projetos).flatMap(project => project).filter(project => project.id === id)
+        if (currentProject) setProject(currentProject[0]);
     }, [id]);
 
+    console.log(project)
     if (!project) {
         return <div>Loading...</div>;
     }
     return (
-        <>
-        </>
+        <Container>
+
+            <ImageBgd style={{ backgroundImage: `url(${project.image})` }} />
+            <ul>
+                {project.iconTecnologics.map((item, index) => {
+                    return (
+                        <li key={index}>
+                            <i className={item.className} color={item.color}></i>
+                        </li>
+                    );
+                })}
+            </ul>
+
+            <TextContent>
+                <h2>{project.title}</h2>
+
+                <div>
+                    <p>{project.descriptionLong}</p>
+                </div>
+
+            </TextContent>
+
+        </Container>
     )
 }
 
--- src/pages/Project/style.tsx
+++ src/pages/Project/style.tsx
@@ -1,1 +1,45 @@
 import styled from "styled-components";
+
+export const Container = styled.section`
+    min-height: 100vh;
+    background-color: var(--pages-background);
+    display: flex;
+    flex-direction: column;
+    justify-content: start;
+    align-items: center;
+    color: var(--pages-color);
+    
+    & ul{
+        display: flex;
+        justify-content: center;
+        align-items: flex-end;
+        gap: 5rem;
+            & li{
+                font-size: 10rem;
+                
+            }
+        }
+    
+`;
+
+export const ImageBgd = styled.div`
+    margin-block: 10rem;
+    background-repeat: no-repeat;
+    background-position: end;
+    background-size: cover;
+    width: 60vw;
+    height: 60vh;
+`;
+export const TextContent = styled.div`
+    text-align: center;
+    & h2{ 
+        font-size: 4.5rem;
+        margin-block: 5rem;
+    }
+    & p{
+        font-size: 3rem;
+    
+    
+    
+    }
+`;
