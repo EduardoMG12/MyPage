eefc7d6dc59db2c677608adc8c2d2605d4628c4c
EduardoMG12 <charles.eduardo.mello.guimaraes@hotmail.com>
2023-01-30 23:56:44 -0300

    feat(): implements more details at layout

--- src/pages/Project/index.tsx
+++ src/pages/Project/index.tsx
@@ -21,8 +21,35 @@ function Project() {
     }
     return (
         <Container>
-
             <ImageBgd style={{ backgroundImage: `url(${project.image})` }} />
+            <TextContent>
+                <h2>{project.title}</h2>
+                <div>{project.descriptionInteger?.length == 0 ?
+                    <p>{project.descriptionInteger}</p>
+                    :
+                    <p>{project.descriptionLong}</p>
+                }
+                    {project.codeAndDeploy.deploy.length == 0 ?
+                        <ExternalLink>
+                            <a href={project.codeAndDeploy.code} target="_blank">
+                                <img className="githubLogo" src={githubLogo} />
+                            </a>
+                        </ExternalLink>
+                        :
+                        <div>
+                            <ExternalLink>
+                                <a href={project.codeAndDeploy.code} target="_blank">
+                                    <img className="githubLogo" src={githubLogo} />
+                                </a>
+                            </ExternalLink>
+                            <ExternalLink>
+                                <a href={project.codeAndDeploy.deploy} target="_blank">
+                                    <img className="vercelLogo1" src={vercelLogoText} alt="" />
+                                </a>
+                            </ExternalLink>
+                        </div>}
+                </div>
+            </TextContent>
             <ul>
                 {project.iconTecnologics.map((item, index) => {
                     return (
@@ -33,37 +60,8 @@ function Project() {
                 })}
             </ul>
 
-            <TextContent>
-                <h2>{project.title}</h2>
 
-                <div>
-                    <p>{project.descriptionInteger}</p>
-                </div>
-
-            </TextContent>
-
-            {project.codeAndDeploy.deploy.length == 0 ?
-                <ExternalLink>
-                    <a href={project.codeAndDeploy.code} target="_blank">
-                        <img className="githubLogo" src={githubLogo} width="10rem" />
-                    </a>
-                </ExternalLink>
-                :
-                <div style={{ display: "flex", alignItems: "center", justifyContent: "space-around", maxWidth: "100vw", width: "40%", flexWrap: "wrap" }}>
-                    <ExternalLink>
-                        <a href={project.codeAndDeploy.code} target="_blank" style={{}}>
-                            <img className="githubLogo" src={githubLogo} width="100rem" />
-                        </a>
-                    </ExternalLink>
-                    <ExternalLink style={{ height: "50%", borderRadius: "0 0 6rem 0", }}>
-                        <a href={project.codeAndDeploy.deploy} target="_blank" style={{ display: "flex", }}>
-                            <img className="vercelLogo1" src={vercelLogoText} alt="" width="150rem" />
-
-                        </a>
-                    </ExternalLink>
-                </div>}
-
-        </Container>
+        </Container >
     )
 }
 
--- src/pages/Project/style.tsx
+++ src/pages/Project/style.tsx
@@ -26,10 +26,13 @@ export const Container = styled.section`
 export const ImageBgd = styled.div`
     margin-block: 10rem;
     background-repeat: no-repeat;
-    background-position: end;
+    background-position: center;
     background-size: cover;
     width: 60vw;
     height: 60vh;
+    @media (max-width:900px) {
+            width: 90vw;
+    }
 `;
 export const TextContent = styled.div`
     text-align: center;
@@ -37,10 +40,46 @@ export const TextContent = styled.div`
         font-size: 4.5rem;
         margin-block: 5rem;
     }
+    & div{
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        min-height: 15vh;
+        width: 90%;
+        max-width: 100vw;
+        margin: 0 auto;
+        & div{
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: space-around;
+            max-width: 100vw;
+            width: 40%;
+        }
     & p{
         font-size: 3rem;
         margin-bottom: 5rem;
         padding-inline: 2rem;
+    }
+    }
+    @media (max-width:900px) {
+        & div{
+        flex-direction: column;
+        justify-content: space-around;
+        align-items: center;
+        min-height: 15vh;
+        width: 90%;
+        max-width: 100vw;
+            & div{
+                flex-direction: row;
+                justify-content: space-around;
+                gap: 1.5rem;
+                max-width: 100vw;
+                width: 100%;
+                flex-wrap: inherit;
+            }
+        }
+        
     }
 `;
 
@@ -48,6 +87,9 @@ export const ExternalLink = styled.div`
     margin-bottom: 5rem;
     &:hover{
         transform: scale(1.05);
+    }
+    & a img{
+        width: 8.5rem;
     }
     transition: 0.4s ease-in-out all;
 `;
